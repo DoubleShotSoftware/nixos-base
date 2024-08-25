b15b4c4bf2a69b22e52e24c5bc4a28dd1dbc4437
AnimusNull <sean@animus.design>
2024-08-25 16:46:20 +0100

    updating gnome keybindings

--- components/linux/desktop/gnome/default.nix
+++ components/linux/desktop/gnome/default.nix
@@ -95,6 +95,7 @@ let
             close = [ "<Super>q" "<Alt>F4" ];
             toggle-fullscreen = [ "<Super>f" ];
             toggle-maximized = [ "<Super>m" ];
+            minimize = [ "" ];
             "move-to-workspace-1" = [ "<Shift><Super>1" ];
             "move-to-workspace-2" = [ "<Shift><Super>2" ];
             "move-to-workspace-3" = [ "<Shift><Super>3" ];
