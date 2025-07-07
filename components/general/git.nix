dfbbe45b932e30da1a0108e566cdd32e9e38c058
AnimusNull <animusnull@gmail.com>
2025-07-07 19:47:17 +0100

    Updating git options

--- components/general/git.nix
+++ components/general/git.nix
@@ -6,6 +6,19 @@ let
   
   gitUserConfigs = mapAttrs (user: userConfig:
     if userConfig.git.enable then
+      let
+        # Generate directory-specific config files
+        dirConfigFiles = mapAttrs' (name: dirCfg:
+          nameValuePair ".config/git/dir-${name}.conf" {
+            text = ''
+              [user]
+              ${optionalString (dirCfg.userName != null) "  name = ${dirCfg.userName}"}
+              ${optionalString (dirCfg.userEmail != null) "  email = ${dirCfg.userEmail}"}
+              ${optionalString (dirCfg.signingKey != null) "  signingkey = ${dirCfg.signingKey}"}
+            '';
+          }
+        ) userConfig.git.dirConfig;
+      in
       (trace "Enabling git configuration for user: ${user}" {
         programs.git = {
           enable = true;
@@ -35,26 +48,15 @@ let
           (if userConfig.git.lazy then [ lazygit ] else []) ++
           (if userConfig.git.jujutsu then [ jujutsu ] else []);
         
-        home.file.".config/git/nixGeneratedExclude.conf".text = 
-          defaultGitIgnore +
-          (if userConfig.git.exclude != "" then "\n# User exclude patterns\n" + userConfig.git.exclude else "") +
-          (if userConfig.git.extraIgnore != "" then "\n# Extra ignore patterns\n" + userConfig.git.extraIgnore else "");
-        
-        home.file.".config/git/local-ignore.conf".text = "";
-      } // (
-        # Generate directory-specific config files
-        foldl' (acc: name:
-          let dirCfg = userConfig.git.dirConfig.${name};
-          in acc // {
-            "home.file.\".config/git/dir-${name}.conf\".text" = ''
-              [user]
-              ${optionalString (dirCfg.userName != null) "  name = ${dirCfg.userName}"}
-              ${optionalString (dirCfg.userEmail != null) "  email = ${dirCfg.userEmail}"}
-              ${optionalString (dirCfg.signingKey != null) "  signingkey = ${dirCfg.signingKey}"}
-            '';
-          }
-        ) {} (attrNames userConfig.git.dirConfig)
-      ))
+        home.file = {
+          ".config/git/nixGeneratedExclude.conf".text = 
+            defaultGitIgnore +
+            (if userConfig.git.exclude != "" then "\n# User exclude patterns\n" + userConfig.git.exclude else "") +
+            (if userConfig.git.extraIgnore != "" then "\n# Extra ignore patterns\n" + userConfig.git.extraIgnore else "");
+          
+          ".config/git/local-ignore.conf".text = "";
+        } // dirConfigFiles;
+      })
     else
       { }
   ) (filterAttrs (user: userConfig: userConfig.userType != "system") users);
