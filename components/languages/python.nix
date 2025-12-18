5812e53dfd722c96104659e12562b7534afd635e
animusnull <animusnull@gmail.com>
2025-12-18 00:25:19 +0000

    bug fix to  pytohn and telescope

--- components/languages/python.nix
+++ components/languages/python.nix
@@ -2,7 +2,7 @@
 { pkgs, username, lib, settings ? {} }:
 {
   packages = with pkgs; [
-    python311Full
+    python311
     python311Packages.pip
     black
     pipenv
--- packages/vimPlugins/telescope-tabs.nix
+++ packages/vimPlugins/telescope-tabs.nix
@@ -1,15 +1,17 @@
 # packages/vimPlugins/telescope-tabs.nix
 # Telescope extension for tab management (LukasPietzschmann/telescope-tabs)
-{ pkgs }:
-let
+{pkgs}: let
   version = "777b1f630f3d6a12a2e71635a82581c988d6da2e";
-in pkgs.vimUtils.buildVimPlugin {
-  inherit version;
-  name = "telescope-tabs";
-  src = pkgs.fetchFromGitHub {
-    owner = "LukasPietzschmann";
-    repo = "telescope-tabs";
-    rev = version;
-    hash = "sha256-5NpH9+0ECrcKi8quPLpCHLSPTuzGETWtq4E+2jqUKio=";
-  };
-}
+in
+  pkgs.vimUtils.buildVimPlugin {
+    inherit version;
+    name = "telescope-tabs";
+    src = pkgs.fetchFromGitHub {
+      owner = "LukasPietzschmann";
+      repo = "telescope-tabs";
+      rev = version;
+      hash = "sha256-5NpH9+0ECrcKi8quPLpCHLSPTuzGETWtq4E+2jqUKio=";
+    };
+    dependencies = with pkgs; [vimPlugins.telescope-nvim];
+    nvimRequireCheck = "telescope";
+  }
