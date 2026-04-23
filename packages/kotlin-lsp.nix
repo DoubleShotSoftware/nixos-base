14c42b35ea210a353c1336a8ec63a7e2ea81c683
AnimusNull <sean@animus.design>
2026-04-23 21:14:52 +0100

    shrink and fix kotlin lsp

--- packages/kotlin-lsp.nix
+++ packages/kotlin-lsp.nix
@@ -1,37 +1,30 @@
 # packages/kotlin-lsp.nix
 # JetBrains Kotlin Language Server
-{ pkgs, lib, stdenv, fetchurl, makeWrapper, jdk17 }:
+{ pkgs, lib, stdenv, fetchzip, makeWrapper, jdk17 }:
 
 let
   version = "262.2310.0";
   platform = if stdenv.isDarwin then "macos-aarch64" else "linux-x64";
+  src = fetchzip {
+    url = "https://download-cdn.jetbrains.com/kotlin-lsp/${version}/kotlin-lsp-${version}-${platform}.zip";
+    hash = "sha256-Bf2qkFpNhQC/Mz563OapmCXeKN+dTrYyQbOcF6z6b48=";
+    stripRoot = false;
+  };
 in
 stdenv.mkDerivation {
   pname = "kotlin-lsp";
   inherit version;
+  inherit src;
 
-  src = fetchurl {
-    url = "https://download-cdn.jetbrains.com/kotlin-lsp/${version}/kotlin-lsp-${version}-${platform}.zip";
-    hash = "sha256-wAQkIVj0teHZF93YSOb2onlIT6WKPivOiEa4B9GtFrE=";
-  };
-
-  nativeBuildInputs = [ pkgs.unzip makeWrapper ];
-  buildInputs = [ jdk17 ];
-
-  unpackPhase = ''
-    unzip $src -d extracted
-  '';
+  dontUnpack = true;
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
-    mkdir -p $out/lib/kotlin-lsp $out/bin
-
-    # Copy the extracted contents. JetBrains publishes the archive with
-    # top-level kotlin-lsp.sh, jre/, lib/, and native/ directories.
-    cp -r extracted/. $out/lib/kotlin-lsp/
-    chmod +x $out/lib/kotlin-lsp/kotlin-lsp.sh
-
-    # Create a stable CLI wrapper around the upstream launcher.
-    makeWrapper $out/lib/kotlin-lsp/kotlin-lsp.sh $out/bin/kotlin-lsp \
+    mkdir -p $out/bin
+    makeWrapper ${pkgs.bash}/bin/bash $out/bin/kotlin-lsp \
+      --add-flags ${src}/kotlin-lsp.sh \
+      --set JAVA_HOME ${jdk17} \
+      --set JDK_HOME ${jdk17} \
       --prefix PATH : ${lib.makeBinPath [ jdk17 ]}
   '';
 
