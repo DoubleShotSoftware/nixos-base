e2aea6a6cdda5bf1ab178667e6755e3a722f5b76
AnimusNull <sean@animus.design>
2026-04-24 19:52:56 +0100

    re-adding ideavimrc; tweaking kotlin lsp; reducing session vars from jvm languages

--- components/languages/kotlin.nix
+++ components/languages/kotlin.nix
@@ -9,9 +9,9 @@
     ktlint
   ];
   sessionVariables = {
-    JAVA_HOME = "${pkgs.jdk25_headless}";
-    JDK_HOME = "${pkgs.jdk25_headless}";
-    IDEA_JDK = "${pkgs.jetbrains.jdk}";
+    # JAVA_HOME = "${pkgs.jdk25_headless}";
+    # JDK_HOME = "${pkgs.jdk25_headless}";
+    # IDEA_JDK = "${pkgs.jetbrains.jdk}";
     _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
   };
   shellPlugins = {
@@ -27,12 +27,12 @@
   permittedInsecurePackages = [
   ];
   homeManager = {
-    # home.file = {
-    #   ".jdks/openjdk11".source = pkgs.jdk11_headless;
-    #   ".jdks/openjdk17".source = pkgs.jdk17_headless;
-    #   ".jdks/openjdk21".source = pkgs.jdk21_headless;
-    #   ".jdks/openjdk25".source = pkgs.jdk25_headless;
-    #   ".jdks/jetbrains".source = pkgs.jetbrains.jdk;
-    # };
+    home.file = {
+      ".jdks/openjdk11".source = pkgs.jdk11_headless;
+      ".jdks/openjdk17".source = pkgs.jdk17_headless;
+      ".jdks/openjdk21".source = pkgs.jdk21_headless;
+      ".jdks/openjdk25".source = pkgs.jdk25_headless;
+      ".jdks/jetbrains".source = pkgs.jetbrains.jdk;
+    };
   };
 }
